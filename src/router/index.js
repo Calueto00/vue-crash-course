fa667b9b5f102ed04f42ad77aa51dce62c2ebd26
Paulo Calueto <paulo.calueto.francisco@gmail.com>
2024-10-11 11:37:09 +0100

    jobview

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NoteFoundView from "@/views/NoteFoundView.vue";
+import JobView from "@/views/JobView.vue";
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +15,13 @@ const router = createRouter({
         path: '/jobs',
         name: 'jobs',
         component: JobsView
-    },{
+    },
+    {
+        path: '/jobs/:id',
+        name: 'job',
+        component: JobView
+    },
+    {
         path: '/:catchAll(.*)',
         name: 'not-found',
         component: NoteFoundView
